ceeb270c39052daa7cd2f00e86a07b10cfda77a1
Gabriel Rennan <gabriel.rennan@hotmail.com>
2024-05-20 14:06:57 -0300

    Desafio Hotel Concluido!

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -16,15 +16,16 @@ namespace DesafioProjetoHospedagem.Models
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            // Implementado
+            if (hospedes.Count<=Suite.Capacidade)
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                // Implementado
+                throw new Exception("O número de hóspedes é maior que a capacidade da suite");
             }
         }
 
@@ -36,8 +37,9 @@ namespace DesafioProjetoHospedagem.Models
         public int ObterQuantidadeHospedes()
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            // *Implementado
+            int qtdHospedes=Hospedes.Count;
+            return qtdHospedes;
         }
 
         public decimal CalcularValorDiaria()
@@ -45,13 +47,15 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados*Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados>=10)
             {
-                valor = 0;
+
+                decimal desconto = valor*0.1M;
+                valor -= desconto ;
             }
 
             return valor;
